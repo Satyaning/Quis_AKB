621b795075ea2385feac7e0c0bf58ca17781832c
SatyaningAndaru <55575075+SatyaningAndaru@users.noreply.github.com>
2021-05-29 11:24:12 +0700

    second commit

--- app/src/main/java/com/example/laporanbulanan/MainActivity.java
+++ app/src/main/java/com/example/laporanbulanan/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.laporanbulanan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+/**
+ * Main Activity (Halaman Main)
+ *
+ * Dikerjakan Pada: 29 Mei 2021
+ * NIM: 10118058
+ * Nama: Satyaning Andaru Bawalaksana
+ * Kelas: IF-2
+ */
 
 public class MainActivity extends AppCompatActivity {
 
